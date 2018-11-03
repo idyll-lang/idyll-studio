c145db8cab521c632ebe55069aa4818c5554779a
Alan Tan <taalantan@gmail.com>
2018-11-03 13:08:12 -0700

    add build

---
+++ build/precache-manifest.d701e66a258808c3dbd6696d39c8d27d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2a7ed8368a2c841c6b74",
+    "url": "./static/js/main.2a7ed836.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "./static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "2a7ed8368a2c841c6b74",
+    "url": "./static/css/main.eeddf9ad.chunk.css"
+  },
+  {
+    "revision": "d30db4cca4e225164f567d6ca3e7ba3e",
+    "url": "./index.html"
+  }
+];
