2644ec7c5ff70f60a37ec1e1205a02e73696cf15
Megan Vo <vvo.megan@gmail.com>
2019-05-19 22:07:58 -0700

    creat metaNode state to persist across views

--- src/render/idyll-display/components/deploy.js
+++ src/render/idyll-display/components/deploy.js
@@ -10,40 +10,53 @@ class Deploy extends React.PureComponent {
   static contextType = Context;
   constructor(props) {
     super(props);
+    this.state = { metaNode: null };
+    this.getMetaNode = this.getMetaNode.bind(this);
   }
 
   componentDidMount() {
     // Grab meta node
-    this.metaNode = idyllAST.getNodesByType(this.context.ast, 'meta')[0];
-    if (!this.metaNode) {
-      // if doesn't exist insert into tree
-      compile('[meta /]').then(metaAST => {
-        let ast = this.context.ast;
-        let lastChild = ast.children[ast.children.length - 1];
+    this.getMetaNode();
+  }
 
-        // reset ids
-        metaAST.children[0].id = getRandomId(); // TextContainer
-        this.metaNode = metaAST.children[0].children[0]; // meta
-        this.metaNode.id = getRandomId();
+  getMetaNode() {
+    let node = idyllAST.getNodesByType(this.context.ast, 'meta')[0];
+    this.setState(
+      {
+        metaNode: node
+      },
+      () => {
+        if (this.state.metaNode == null) {
+          // if doesn't exist insert into tree
+          compile('[meta /]').then(metaAST => {
+            let ast = this.context.ast;
+            let lastChild = ast.children[ast.children.length - 1];
 
-        if (lastChild.name === 'TextContainer') {
-          // push just meta tag
-          lastChild.children.push(metaAST.children[0].children[0]);
-        } else {
-          // push text container
-          ast = idyllAST.appendNode(ast, metaAST.children[0]);
+            // reset ids
+            metaAST.children[0].id = getRandomId(); // TextContainer
+            this.setState({ metaNode: metaAST.children[0].children[0] }); // meta
+            this.state.metaNode.id = getRandomId();
+
+            if (lastChild.name === 'TextContainer') {
+              // push just meta tag
+              lastChild.children.push(metaAST.children[0].children[0]);
+            } else {
+              // push text container
+              ast = idyllAST.appendNode(ast, metaAST.children[0]);
+            }
+            this.context.setAst(this.context.ast);
+          });
         }
-        this.context.setAst(this.context.ast);
-      });
-    }
+      }
+    );
   }
 
   handleUpdateValue(propName, e) {
     // if index.idyll doesn't have this prop for meta tag, insert one
-    if (!(propName in this.metaNode.properties)) {
-      this.metaNode.properties[propName] = { type: 'value', value: '' };
+    if (!(propName in this.state.metaNode.properties)) {
+      this.state.metaNode.properties[propName] = { type: 'value', value: '' };
     }
-    this.metaNode.properties[propName].value = e.target.value;
+    this.state.metaNode.properties[propName].value = e.target.value;
     this.context.setAst(this.context.ast);
 
     // Debug
@@ -60,8 +73,8 @@ class Deploy extends React.PureComponent {
         }}
         type='text'
         defaultValue={
-          this.metaNode && propName in this.metaNode.properties
-            ? this.metaNode.properties[propName].value
+          this.state.metaNode && propName in this.state.metaNode.properties
+            ? this.state.metaNode.properties[propName].value
             : null
         }
       />
@@ -86,14 +99,3 @@ class Deploy extends React.PureComponent {
 }
 
 export default Deploy;
-
-// this.metaNode
-//   ? Object.keys(this.metaNode.properties).map(propName => {
-//       return (
-//         <div key={propName} style={{ display: 'flex', flexDirection: 'row' }}>
-//           {propName}
-//           {this.renderProps(propName)}
-//         </div>
-//       );
-//     })
-//   : null;
