65cc4d3504261e97c56f88873b6f692dd128362f
Megan Vo <vvo.megan@gmail.com>
2020-02-23 22:15:52 -0800

    wip property test

--- test/components.test.js
+++ test/components.test.js
@@ -3,14 +3,38 @@ import { mount } from 'enzyme';
 import { Property } from '../src/render/idyll-display/components/property';
 import expect from 'expect';
 
-describe('<Property />', () => {
+describe('<Property /> with props', () => {
   let component;
+  let value = '';
+  let nextType = '';
+  let updateProperty = jest.fn((propertyName, value, e) => {
+    value = value;
+  });
+
+  let updateNodeType = jest.fn((name, nextType) => {
+    nextType = nextType;
+  });
+
+  let updateShowPropDetailsMap = jest.fn();
 
   beforeAll(() => {
-    component = mount(<Property />);
+    value = '';
+    nextType = '';
+
+    component = mount(
+      <Property
+        updateProperty={updateProperty}
+        name={'author'}
+        value={{ value: value }}
+        updateNodeType={updateNodeType}
+        variableData={{}}
+        updateShowPropDetailsMap={updateShowPropDetailsMap}
+        showDetails={false}
+        activePropName={''}
+        cursorPosition={-1}
+      />
+    );
   });
 
-  it('prelim snapshot test', () => {
-    expect(component.html()).toMatchSnapshot();
-  });
+  it('prelim snapshot test', () => {});
 });
