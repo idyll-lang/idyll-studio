8072329e1be0d6b3ca44a0fd928594999caf77d2
Matthew Conlen <mc@mathisonian.com>
2021-02-16 20:43:18 -0800

    try no password

--- add-osx-cert.sh
+++ add-osx-cert.sh
@@ -15,7 +15,7 @@ security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
 security unlock-keychain -p actions $KEY_CHAIN
 
-security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+security import $CERTIFICATE_P12 -k $KEY_CHAIN -P "" -T /usr/bin/codesign;
 
 security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 
