0886434799075a250f533c751c1482740048a39d
Matthew Conlen <mc@mathisonian.com>
2021-03-10 20:26:34 -0800

    re-enable osx  code sign

--- forge.config.js
+++ forge.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   "packagerConfig": {
     "icon": "static/icon.icns",
-    // "osxSign": {
-    //   "entitlements": "static/entitlements.plist",
-    //   "entitlements-inherit": "static/entitlements.plist",
-    //   "gatekeeper-assess": false,
-    //   "hardenedRuntime": true,
-    //   "identity": "Developer ID Application: Matthew Conlen (9NELR4LFNY)"
-    // },
-    // "osxNotarize": {
-    //   "appleId": process.env['APPLE_ID'],
-    //   "appleIdPassword": process.env['APPLE_ID_PASSWORD']
-    // }
+    "osxSign": {
+      "entitlements": "static/entitlements.plist",
+      "entitlements-inherit": "static/entitlements.plist",
+      "gatekeeper-assess": false,
+      "hardenedRuntime": true,
+      "identity": "Developer ID Application: Matthew Conlen (9NELR4LFNY)"
+    },
+    "osxNotarize": {
+      "appleId": process.env['APPLE_ID'],
+      "appleIdPassword": process.env['APPLE_ID_PASSWORD']
+    }
   },
   "makers": [
     {
